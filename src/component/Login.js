207d38d997d7e5a9bff42867323a564a3999e670
anjali <anjalipaswan@innotechteam.in>
2023-01-28 17:53:23 +0530

    sign up

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./login.css";
+// import "./login.css";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
--- src/component/Signup.js
+++ src/component/Signup.js
@@ -3,19 +3,35 @@ import { useSelector, useDispatch } from "react-redux";
 import { getRoles, signUpData } from "../api";
 import "./sign.css";
 function Signup() {
+  const [error, setError] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const dispatch = useDispatch();
   const [selectedRole, setSelectedRole] = useState("");
   const roleList = useSelector((state) => state.pollingReducer.roleList);
   const roles = Object.values(roleList);
+  const dispatch = useDispatch();
   useEffect(() => {
     getRoles(dispatch);
   }, []);
+  const emailRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (!firstName || !lastName || !email || !password || !selectedRole) {
+      setError("All fields are required.");
+      return;
+    }
+
+    if (!emailRegex.test(email)) {
+      setError("Invalid email address.");
+      return;
+    }
+    if (password.length < 8) {
+      setError("Password must be at least 8 characters long.");
+      return;
+    }
     signUpData(dispatch, {
       firstName,
       lastName,
@@ -24,6 +40,32 @@ function Signup() {
       selectedRole,
     });
   };
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   if (!firstName || !lastName || !email || !password || !selectedRole) {
+  //     setError("All fields are required.");
+  //     return;
+  //   }
+  //   signUpData(dispatch, {
+  //     firstName,
+  //     lastName,
+  //     email,
+  //     password,
+  //     selectedRole,
+  //   });
+  // };
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   signUpData(dispatch, {
+  //     firstName,
+  //     lastName,
+  //     email,
+  //     password,
+  //     selectedRole,
+  //   });
+  // };
   return (
     <div className="signup-main-comtainer">
       <h3>Signup</h3>
@@ -81,6 +123,7 @@ function Signup() {
               ))}
             </select>
           </label>
+          {error !== "" && <p className="error-message">{error}</p>}
           <button type="submit">Submit</button>
         </form>
       </div>
